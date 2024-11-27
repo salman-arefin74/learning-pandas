63ab461de6f2dccd6cc4034e37e726467b92f321
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2024-11-27 11:35:02 +0600

    Solved https://leetcode.com/problems/reshape-data-concatenate/

---
+++ reshape-data-concatenate.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2]) # column concatenate
+
+    # row concatenate:
+    # return pd.concat([df1, df2], axis=1)
